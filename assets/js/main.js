aab1290175dca5481bc7bc5822ef420310042115
Lapi_Kun <Lapi_Kun@LAPIKUN>
2023-06-30 23:28:31 +0800

    removing unnecessary files

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,52 +140,6 @@
     }
   });
 
-  // /**
-  //  * Skills animation
-  //  */
-  // let skilsContent = select('.skills-content');
-  // if (skilsContent) {
-  //   new Waypoint({
-  //     element: skilsContent,
-  //     offset: '80%',
-  //     handler: function (direction) {
-  //       let progress = select('.progress .progress-bar', true);
-  //       progress.forEach((el) => {
-  //         el.style.width = el.getAttribute('aria-valuenow') + '%'
-  //       });
-  //     }
-  //   })
-  // }
-
-  // /**
-  //  * Testimonials slider
-  //  */
-  // new Swiper('.testimonials-slider', {
-  //   speed: 600,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20
-  //     },
-
-  //     1200: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 20
-  //     }
-  //   }
-  // });
-
   /**
    * Porfolio isotope and filter
    */
@@ -229,27 +183,5 @@
   //   width: '90%',
   //   height: '90vh'
   // });
-
-  // /**
-  //  * Portfolio details slider
-  //  */
-  // new Swiper('.portfolio-details-slider', {
-  //   speed: 400,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   }
-  // });
-
-  // /**
-  //  * Initiate Pure Counter 
-  //  */
-  // new PureCounter();
 
 })()
